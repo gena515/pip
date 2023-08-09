b02915a14662e628f5bbcefcbdc7fe486e57d093
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2023-08-09 18:39:53 -0400

    make LinkMetadataCache
    - catch an exception when parsing metadata which only occurs in CI
    - handle --no-cache-dir
    - call os.makedirs() before writing to cache too
    - catch InvalidSchema when attempting git urls with BatchDownloader
    - fix other test failures
    - reuse should_cache(req) logic
    - gzip compress link metadata for a slight reduction in disk space
    - only cache built sdists
    - don't check should_cache() when fetching
    - cache lazy wheel dists
    - add news
    - turn debug logs in fetching from cache into exceptions
    - use scandir over listdir when searching normal wheel cache
    - handle metadata email parsing errors
    - correctly handle mutable cached requirement
    - use bz2 over gzip for an extremely slight improvement in disk usage

--- src/pip/_internal/req/req_install.py
+++ src/pip/_internal/req/req_install.py
@@ -97,7 +97,7 @@ class InstallRequirement:
         permit_editable_wheels: bool = False,
     ) -> None:
         assert req is None or isinstance(req, Requirement), req
-        self.req = req
+        self._req = req
         self.comes_from = comes_from
         self.constraint = constraint
         self.editable = editable
@@ -209,6 +209,23 @@ class InstallRequirement:
 
         # This requirement needs to be unpacked before it can be installed.
         self._archive_source: Optional[Path] = None
+
+    @property
+    def req(self) -> Optional[Requirement]:
+        """Calculate a requirement from the cached dist, if populated.
+
+        The cached dist can be populated by either
+        ``self.cache_virtual_metadata_only_dist()`` or
+        ``self.cache_concrete_dist()`` and can also be retrieved with
+        ``self.get_dist()``."""
+        if self._req is not None:
+            return self._req
+        if self._dist is not None:
+            name = self._dist.canonical_name
+            version = str(self._dist.version)
+            self._req = Requirement(f"{name}=={version}")
+            return self._req
+        return None
 
     def __str__(self) -> str:
         if self.req:
@@ -399,7 +416,7 @@ class InstallRequirement:
 
     def _set_requirement(self) -> None:
         """Set requirement after generating metadata."""
-        assert self.req is None
+        assert self._req is None
         assert self.metadata is not None
         assert self.source_dir is not None
 
@@ -409,7 +426,7 @@ class InstallRequirement:
         else:
             op = "==="
 
-        self.req = get_requirement(
+        self._req = get_requirement(
             "".join(
                 [
                     self.metadata["Name"],
@@ -435,7 +452,7 @@ class InstallRequirement:
             metadata_name,
             self.name,
         )
-        self.req = get_requirement(metadata_name)
+        self._req = get_requirement(metadata_name)
 
     def check_if_exists(self, use_user_site: bool) -> None:
         """Find an installed distribution that satisfies or conflicts
